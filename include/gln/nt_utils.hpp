53fbdbcf3743e08375be09e016a8763926cee71a
Carmen Azorin Marti <74839760+carmenazorinm@users.noreply.github.com>
2025-11-18 18:28:45 +0100

    Fix formatting of function declarations in nt_utils.hpp

--- include/gln/nt_utils.hpp
+++ include/gln/nt_utils.hpp
@@ -3,7 +3,7 @@
 
 namespace gln {
 
-BigInt mod(const BigInt& a, const BigInt& m);        // en [0, m-1]
+BigInt mod(const BigInt& a, const BigInt& m);  
 BigInt addmod(const BigInt& a, const BigInt& b, const BigInt& m);
 BigInt submod(const BigInt& a, const BigInt& b, const BigInt& m);
 BigInt mulmod(const BigInt& a, const BigInt& b, const BigInt& m);
@@ -11,10 +11,10 @@ uint32_t to_u32(const BigInt& x);
 
 BigInt gcd(BigInt a, BigInt b);
 void   egcd(const BigInt& a, const BigInt& b, BigInt& x, BigInt& y, BigInt& g);
-BigInt invmod(const BigInt& a, const BigInt& m);     // lanza si no invertible
+BigInt invmod(const BigInt& a, const BigInt& m);   
 
 struct TreeaOut { BigInt v; BigInt r; };
-TreeaOut treea(BigInt g, BigInt s);                  // TrEEA
+TreeaOut treea(BigInt g, BigInt s);     
 
 bool   is_probable_prime(const BigInt& n, int rounds=32);
 BigInt next_prime(const BigInt& start);
