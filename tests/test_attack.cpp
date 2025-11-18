cc02a0e2dc7d6ad76dde2a0c567129f3405595cd
Carmen Azorin Marti <74839760+carmenazorinm@users.noreply.github.com>
2025-11-18 19:21:12 +0100

    Update test_attack.cpp

--- tests/test_attack.cpp
+++ tests/test_attack.cpp
@@ -1,22 +1,24 @@
+#include <cassert>
 #include <iostream>
+#include <stdexcept>
+#include <vector>
+
 #include "gln/keygen.hpp"
 #include "gln/attack.hpp"
 #include "gln/params.hpp"
 
 using namespace gln;
 
-int main(int argc, char** argv){
-  Params prm = choose_params(/*n*/10, /*t_w*/2, /*z*/(16), /*beta*/3);
-  Random rng; 
+static void test_basic() {
+  Params prm = choose_params(/*n*/5, /*t_w*/3, /*z*/2, /*beta*/4);
+  Random rng;
   KeyPair kp = keygen(prm, rng);
 
-  // Ataca sobre los 3 primeros índices (i=0,j=1,k=2), y prueba 2000 candidatos
-  AttackReport rep = attack_triples(kp.pk, {0,1,2}, prm.b, prm.beta, /*max_candidates*/2000);
+  AttackReport rep = attack_triples(kp.pk, prm.b, prm.beta);
+  assert(rep.found_exact = true);
+}
 
-  std::cout << "Combos probados: " << rep.combos_tested << "\n";
-  std::cout << "g_guess: " << rep.g_guess << "\n";
-  std::cout << "g real: " << kp.pk.g << "\n";
-  std::cout << "found_exact? " << ((rep.g_guess == kp.pk.g || (rep.g_guess % kp.pk.g)==BigInt(0)) ? "YES":"NO") << "\n";
+int main(){
+  test_basic();
   return 0;
-
 }
