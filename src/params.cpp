e1b80e695c7cc892efc3846c207141a9805287bb
Carmen Azorin Marti <74839760+carmenazorinm@users.noreply.github.com>
2025-11-18 18:25:46 +0100

    Update params.cpp

--- src/params.cpp
+++ src/params.cpp
@@ -17,7 +17,6 @@ Params choose_params(std::size_t n, std::size_t t_w, std::size_t z, std::size_t 
     // ell = ceil(log2(t_w))
     P.ell = (t_w <= 1) ? 1 : static_cast<std::size_t>(std::ceil(std::log2(static_cast<double>(t_w))));
 
-    // bits_g segun formula del paper
     std::size_t term1 = 2 * t_w * (P.b + P.beta) + 3;
     std::size_t term2 = 2 * (t_w * (P.b + P.beta) + P.ell - P.beta) + 3;
     P.bits_g = std::max(term1, term2);
